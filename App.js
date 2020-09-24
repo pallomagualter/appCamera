446ea78f6759f7f0425bc43cab36f51ae008b018
Palloma Gualter <pallomagualter@gmail.com>
2020-09-24 16:07:59 -0300

    expo-camera

--- App.js
+++ App.js
@@ -1,13 +1,34 @@
-import React from 'react';
-import { StyleSheet, Text, Image, View, SafeAreaView } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { Camera } from 'expo-camera';
 
 export default function App() {
+  const [type, setType] = useState(Camera.Constants.Type.back); //Aqui iremos definir se o app irá iniciar utilizando a camera frontal ou back, definindo assim o tipo
+  const [hasPermission, setHasPermission] = useState(null); //Para armazenar o estado da permissão de utilização da camera
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted'); //Aqui ele irá pegar essa permissão e armazenar como permitido
+    })
+  }, []);
+
+  if(hasPermission === null){
+    return <View />
+  }
+
+  if(hasPermission === false){
+    return <Text>Acesso negado!</Text>
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <View>
-        <Image />
+        <Camera
+        style={{ flex: 1  }}
+        type={type}
+        />
       </View>
-       <Text style={styles.textContainer}>Hi, baby!</Text>
     </SafeAreaView>
   );
 }
@@ -15,13 +36,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#7159c1',
+    //backgroundColor: '#7159c1',
     justifyContent: 'center',
     alignItems: 'center',
-  },
-  textContainer: {
-    color: '#fff',
-    fontWeight: 'bold',
-    fontSize: 24,
   },
 });
