e06568f54df28ed98a5309b02653668d37e52864
Palloma Gualter <pallomagualter@gmail.com>
2020-09-25 12:04:37 -0300

    button take a photo

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import { SafeAreaView, StyleSheet, View } from 'react-native';
+import React, { useEffect, useState, useRef } from 'react';
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Feather, FontAwesome } from '@expo/vector-icons';
+
 import { Camera } from 'expo-camera';
 
 export default function App() {
+  const camRef = useRef(null);
   const [type, setType] = useState(Camera.Constants.Type.back); //Aqui iremos definir se o app irá iniciar utilizando a camera frontal ou back, definindo assim o tipo
   const [hasPermission, setHasPermission] = useState(null); //Para armazenar o estado da permissão de utilização da camera
+  const [capturedPhoto, setCapturedPhoto] = useState(null); //Armazena a foto capturada a uri
+  const [open, setOpen] = useState(false); 
 
   useEffect(() => { //quando ele abrir pela primeira vez ele dispara uma ação para o usuário permitir a utilização da câmera
     (async () => {
@@ -21,12 +26,47 @@ export default function App() {
     return <Text>Acesso Negado!</Text>
   }
 
+  async function takePicture(){
+    if(camRef){ //primeiro verifica se há uma camera
+      const data = await camRef.current.takePictureAsync(); //salva os dados da camera
+      setCapturedPhoto(data.uri);
+      console.log(data); 
+    }
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <Camera
         style={{ flex: 1 }}
         type={type}
-      />
+        ref={camRef} //faz referência a camera do celular
+      >
+        <View style={{ flex: 1, backgroundColor: 'transparent', flexDirection: 'row' }}>
+          <TouchableOpacity
+            style={{
+              position: 'absolute',
+              bottom: 20,
+              left: 20,
+            }}
+            onPress={ () => {
+              setType(
+                type === Camera.Constants.Type.back
+                ? Camera.Constants.Type.front
+                : Camera.Constants.Type.back
+              );
+            }}
+          >
+              <Feather name="refresh-cw" size={23} color="#fff"/>
+              <Text style={{ fontSize: 20, marginBottom: 13, color: '#fff', fontWeight: 'bold' }}>
+                Trocar
+              </Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+
+      <TouchableOpacity style={styles.button} onPress={ takePicture} >
+        <FontAwesome name= "camera" size={23} color="#fff" />
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
@@ -36,5 +76,15 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     justifyContent: 'center',
+  },
+
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#121212',
+    margin: 20,
+    borderRadius: 10,
+    margin: 20,
+    height: 50,
   },
 });
