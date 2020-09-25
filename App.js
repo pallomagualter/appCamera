9c69047034e18152d033c3c3dac5f857c88ed19d
Palloma Gualter <pallomagualter@gmail.com>
2020-09-25 10:40:54 -0300

    Camera permission

--- App.js
+++ App.js
@@ -6,9 +6,9 @@ export default function App() {
   const [type, setType] = useState(Camera.Constants.Type.back); //Aqui iremos definir se o app irá iniciar utilizando a camera frontal ou back, definindo assim o tipo
   const [hasPermission, setHasPermission] = useState(null); //Para armazenar o estado da permissão de utilização da camera
 
-  useEffect(() => {
+  useEffect(() => { //quando ele abrir pela primeira vez ele dispara uma ação para o usuário permitir a utilização da câmera
     (async () => {
-      const { status }= await Camera.requestPermissionsAsync();
+      const { status }= await Camera.requestPermissionsAsync(); //Pegando o status e salvando a useState
       setHasPermission(status === 'granted'); //Aqui ele irá pegar essa permissão e armazenar como permitido
     })();
   }, []);
@@ -18,7 +18,7 @@ export default function App() {
   }
   
   if(hasPermission === false){
-    return <View />
+    return <Text>Acesso Negado!</Text>
   }
 
   return (
