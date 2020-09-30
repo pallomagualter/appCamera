29ddd4fcce82e6ab86aef0b8f0c5ebfc8b414e4d
Palloma Gualter <pallomagualter@gmail.com>
2020-09-30 16:07:46 -0300

    Home

---
+++ src/pages/Home/styles.js
@@ -0,0 +1,17 @@
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      justifyContent: 'center',
+    },
+  
+    button: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#121212',
+      margin: 20,
+      borderRadius: 10,
+      margin: 20,
+      height: 50,
+    },
+  });
---
+++ src/styles.js
@@ -0,0 +1,8 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+   container: {
+      textAlign: 'center',
+      backgroundColor: '#7159c1',
+   },
+})
