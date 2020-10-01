796f9db9f9e4263a355dae8cb3e98381e478d8cc
Palloma Gualter <pallomagualter@gmail.com>
2020-10-01 10:06:28 -0300

    Login style

---
+++ src/pages/Login/styles.js
@@ -0,0 +1,55 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#121212',    
+    },
+
+    form: {
+        alignSelf: 'stretch', //para ocupar a largura inteira
+        paddingHorizontal: 30, //propiedade apenas do native é padding apenas na horizontal
+        marginTop: 30,
+    },
+
+    label: {
+        fontWeight: 'bold',
+        color: '#444',
+        marginBottom: 8,
+    },
+
+    input: {
+        borderWidth: 1,
+        borderColor: '#ddd',
+        paddingHorizontal: 20,
+        fontSize: 18,
+        color: '#444',
+        height: 44,
+        marginBottom: 20,
+        borderRadius: 2,
+        backgroundColor: '#fff',
+    },
+
+    button: {
+        height: 42,
+        backgroundColor: '#2CBF9A',//
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 4,
+     
+     },
+ 
+     buttonText: {
+         color: '#121212',
+         fontWeight: 'bold',
+         fontSize: 18,
+     },
+
+     footer: {
+        marginTop: 42,
+     },
+ 
+
+});
